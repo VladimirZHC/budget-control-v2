2bae8a16a6de93bcdc365d725f44c41eb64f1c0a
Vladimir <Sluhaivova@gmail.com>
2022-02-27 16:06:25 +0300

    update views.py and add requirements.txt

--- budgetcontrol/control/urls.py
+++ budgetcontrol/control/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from control import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('operations/', views.operation_list),
     path('operations/<int:pk>/', views.operation_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- budgetcontrol/control/views.py
+++ budgetcontrol/control/views.py
@@ -1,49 +1,45 @@
-
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Operation
 from .serializers import ControlSerializer
 
 
 
-@csrf_exempt
-def operation_list(request):
+@api_view(['GET', 'POST'])
+def operation_list(request, format=None):
     if request.method == 'GET':
         operations = Operation.objects.all()
         serializer = ControlSerializer(operations, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ControlSerializer(data=data)
+        serializer = ControlSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 
 
-@csrf_exempt
-def operation_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def operation_detail(request, pk, format=None):
     try:
         operation = Operation.objects.get(pk=pk)
     except Operation.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ControlSerializer(operation)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ControlSerializer(data=data)
+        serializer = ControlSerializer(operation, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         operation.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
