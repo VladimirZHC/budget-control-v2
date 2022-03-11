862a50e2978170f6a979f7e12c874fe1be7aa40d
Vladimir <Sluhaivova@gmail.com>
2022-03-11 12:55:18 +0300

    +features

--- budgetcontrol/control/serializers.py
+++ budgetcontrol/control/serializers.py
@@ -28,12 +28,12 @@ class ControlSerializer(serializers.ModelSerializer):
         
 
 class HistoryOperationSerializer(serializers.ModelSerializer):
+    operation_id = serializers.CharField()
     history_id = serializers.CharField(source='id')
     up_day = serializers.DateTimeField(format='%H:%M:%S %d/%m/%Y')
-    # tags = serializers.ListField()
     class Meta:
         model = HistoryOperation
-        fields = ('operation', 'history_id', 'title', 'up_day', 'transaction', 'tags')
+        fields = ('operation_id', 'history_id', 'title', 'up_day', 'transaction', 'tags')
         
         
 
--- budgetcontrol/control/signals.py
+++ budgetcontrol/control/signals.py
@@ -1,17 +1,19 @@
+
 from .models import Operation, HistoryOperation
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import json
 
 
 @receiver(post_save, sender=Operation)
 def create_history(sender, instance, created, **kwargs):
-    if created:
-        HistoryOperation.objects.create(
-            operation = instance,
-            title = instance.title,
-            transaction=instance.transaction,
-            tags = instance.tags                     
-            )
+    HistoryOperation.objects.create(
+        operation = instance,
+        title = instance.title,
+        transaction=instance.transaction,
+        tags = instance.tags             
+        )
+    
 
 # @receiver(post_save, sender=Operation)
 # def save_history(sender, instance, **kwargs):
