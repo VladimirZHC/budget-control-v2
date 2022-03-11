23df3e75fc3b731c3113de3bf7058d6d479254eb
Vladimir <Sluhaivova@gmail.com>
2022-03-11 14:16:06 +0300

    add features

--- budgetcontrol/control/signals.py
+++ budgetcontrol/control/signals.py
@@ -1,8 +1,7 @@
-
 from .models import Operation, HistoryOperation
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-import json
+
 
 
 @receiver(post_save, sender=Operation)
